c5ab787d4d71edd116ef59f81406f0d54b100a6d
Nderitu10 <raynderitu10@gmail.com>
2025-01-05 23:38:37 +0300

    updated movie card

---
+++ src/pages/favorites.jsx
@@ -0,0 +1,8 @@
+function Favorite() {
+    return <div className="favorites-empty">
+        <h2>There are no favorite movies</h2>
+        <p>Start adding movies to your favorites!</p>
+    </div>
+}
+
+export default Favorite
