e75caf697d6ffc2d5997f8bbac49d818ae52d9e2
Nderitu10 <raynderitu10@gmail.com>
2025-01-06 21:33:08 +0300

    Set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
-import MovieCard from './components/movieCard'
+import Favorites from './pages/favorites'
 import Home from './pages/home'
+import {Routes, Route} from "react-router-dom"
 
 function App() {
 
   return (
-    <>
-    <Home />
-    </>
+    <main className = "main-content">
+      <Routes>
+        <Route path = "/" element = {<Home/>}/>
+        <Route path = "/favorites" element = {<Favorites/>}/>
+      </Routes>
+
+    </main>
+
   )
 }
 
--- src/pages/favorites.jsx
+++ src/pages/favorites.jsx
@@ -1,8 +1,8 @@
-function Favorite() {
+function Favorites() {
     return <div className="favorites-empty">
         <h2>There are no favorite movies</h2>
         <p>Start adding movies to your favorites!</p>
     </div>
 }
 
-export default Favorite
+export default Favorites
